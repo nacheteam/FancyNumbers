b33dee405c615399ae2789105cf35faa93049cfe
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-01-22 20:33:13 +0100

    Discover pythagoric triples

--- pythagorean_triple.py
+++ pythagorean_triple.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 
 def checkPythagorasTriple(triple):
     '''
@@ -16,4 +17,16 @@ def checkPythagorasTripleArray(triple_array):
     @param triple_array Numpy array with the triples to be checked
     @return Returns an array with true or false for each triple introduced
     '''
-    return np.power(np.choose(0,triple_array),2) + np.power(np.choose(1,triple_array),2) == np.power(np.choose(2,triple_array),2)
+    return np.power(np.array([t[0] for t in triple_array]),2) + np.power(np.array([t[1] for t in triple_array]),2) == np.power(np.array([t[2] for t in triple_array]),2)
+
+def discoverPythagorasTriples(limit):
+    '''
+    @brief Function that discovers Pythagoras triples up to the maximum value of
+    limit in one of the positions a,b,c. It prints the Pythagoras triples discovered.
+    @param limit Integer that represents the limit for a,b, and c
+    '''
+    all_choices = np.array(list(itertools.product(list(range(limit)), repeat=3)))
+    results = checkPythagorasTripleArray(all_choices)
+    for i in range(len(results)):
+        if results[i]:
+            print("The triple (" + str(all_choices[i][0]) + "," + str(all_choices[i][1]) + "," + str(all_choices[i][2]) + ") is Pythagoric")
