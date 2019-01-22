0e6b82ffc38d4f9fabfcf713e387a1a5dbc3c86f
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-01-22 20:14:23 +0100

    Function to check array of pythagoras triples

--- pythagorean_triple.py
+++ pythagorean_triple.py
@@ -8,3 +8,12 @@ def checkPythagorasTriple(triple):
     or not
     '''
     return triple[0]**2 + triple[1]**2 == triple[2]**2
+
+def checkPythagorasTripleArray(triple_array):
+    '''
+    @brief Function that uses numpy to check in a vectorized way wether the triples
+    in triple_array are Pythagoras triples.
+    @param triple_array Numpy array with the triples to be checked
+    @return Returns an array with true or false for each triple introduced
+    '''
+    return np.power(np.choose(0,triple_array),2) + np.power(np.choose(1,triple_array),2) == np.power(np.choose(2,triple_array),2)
